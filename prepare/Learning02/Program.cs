40380a870656cb3fef137c43a107fe82ec77452a
c-moo1234 <moo23028@byui.edu>
2024-11-20 10:44:22 -0700

    W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myJob = new Job();
+        myJob._company = "Tche";
+        myJob._jobTitle = "Gaucho";
+        myJob._startYear = 2023;
+        myJob._endYear = 2024;
+        // myJob.Display();
+
+        Job myJob2 = new Job();
+        myJob2._company = "Cinamark";
+        myJob2._jobTitle = "Cashier";
+        myJob2._startYear = 2015;
+        myJob2._endYear = 2020;
+
+        Job myJob3 = new Job();
+        myJob3._company = "Delta";
+        myJob3._jobTitle = "Baggage Handler";
+        myJob3._startYear = 2020;
+        myJob3._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Cyrus";
+        myResume._jobs.Add(myJob);
+        myResume._jobs.Add(myJob2);
+        myResume._jobs.Add(myJob3);
+        myResume.Display();
     }
 }
